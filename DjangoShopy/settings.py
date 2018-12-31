d1ce81116f225dbddc4d7ebc5e5f58600854ce6e
david <david_drcr@hotmail.com>
2018-12-31 17:56:41 -0500

    Aun falta ver como pasa al html exacta,mente

--- DjangoShopy/settings.py
+++ DjangoShopy/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'DjangoShopy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'products/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- products/views.py
+++ products/views.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Product
 # Create your views here.
+
+
+def hello_world(request):
+    # return render(request, 'index.html')
+
+    product = Product.objects.order_by('id')
+    template = loader.get_template("index.html")
+    context = {
+        'product': product
+    }
+    return HttpResponse(template.render(context, request))
+    # Revisar como enviaba antes a un archivo index.html los datos desde aqui
